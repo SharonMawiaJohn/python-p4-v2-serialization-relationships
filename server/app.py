6a56e5b6008c5dc4bca904ce05d3d440e090672c
SharonMawiaJohn <sharonmawia76@gmail.com>
2024-10-03 00:21:02 +0300

    Adds changes made

--- server/app.py
+++ server/app.py
@@ -1,62 +1,42 @@
 # server/app.py
+#!/usr/bin/env python3
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Zookeeper, Enclosure, Animal
+from models import db, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Zoo app</h1>'
-
-
-@app.route('/animal/<int:id>')
-def animal_by_id(id):
-    animal = Animal.query.filter(Animal.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {animal.id}</ul>'
-    response_body += f'<ul>Name: {animal.name}</ul>'
-    response_body += f'<ul>Species: {animal.species}</ul>'
-    response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
-    response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
-
-    return make_response(response_body)
-
-
-@app.route('/zookeeper/<int:id>')
-def zookeeper_by_id(id):
-    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {zookeeper.id}</ul>'
-    response_body += f'<ul>Name: {zookeeper.name}</ul>'
-    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
-
-    for animal in zookeeper.animals:
-        response_body += f'<ul>Animal: {animal.name}</ul>'
-
-    return make_response(response_body)
-
-
-@app.route('/enclosure/<int:id>')
-def enclosure_by_id(id):
-    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
-    response_body = f''
-    response_body += f'<ul>ID: {enclosure.id}</ul>'
-    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
-    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
-
-    for animal in enclosure.animals:
-        response_body += f'<ul>Animal: {animal.name}</ul>'
-
-    return make_response(response_body)
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+    return make_response(body, status)     
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets= []
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body = {'count': len(pets),
+            'pets': pets}    
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python3
-
+#server/seed.py
 from random import choice as rc
-
 from faker import Faker
 
 from app import app
-from models import db, Zookeeper, Animal, Enclosure
-
-fake = Faker()
+from models import db, Pet
 
 with app.app_context():
 
-    Animal.query.delete()
-    Zookeeper.query.delete()
-    Enclosure.query.delete()
+    # Create and initialize a faker generator
+    fake = Faker()
 
-    zookeepers = []
-    for n in range(25):
-        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
-            start_date='-70y', end_date='-18y'))
-        zookeepers.append(zk)
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
 
-    db.session.add_all(zookeepers)
+    # Create an empty list
+    pets = []
 
-    enclosures = []
-    environments = ['Desert', 'Pond', 'Ocean',
-                    'Field', 'Trees', 'Cave', 'Cage']
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
 
-    for n in range(25):
-        e = Enclosure(environment=rc(environments),
-                      open_to_visitors=rc([True, False]))
-        enclosures.append(e)
+    # Add some Pet instances to the list
+    for n in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
 
-    db.session.add_all(enclosures)
+    # Insert each Pet in the list into the "pets" table
+    db.session.add_all(pets)
 
-    animals = []
-    species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
-               'Snake', 'Monkey']
-
-    for n in range(200):
-        name = fake.first_name()
-        while name in [a.name for a in animals]:
-            name = fake.first_name()
-        a = Animal(name=name, species=rc(species))
-        a.zookeeper = rc(zookeepers)
-        a.enclosure = rc(enclosures)
-        animals.append(a)
-
-    db.session.add_all(animals)
+    # Commit the transaction
     db.session.commit()
